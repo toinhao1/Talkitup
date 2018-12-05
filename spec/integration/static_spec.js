d610b28e94223697184c6d3476840534c4277f76
toinhao1 <toinhao89@gmail.com>
2018-12-05 06:57:06 -0600

    Assignment Express Intro Complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -12,4 +12,12 @@ describe("routes : static", () => {
       });
     });
   });
+  describe("GET /marco", () => {
+    it("should return status code 200", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      })
+    })
+  })
 });
--- src/routes/static.js
+++ src/routes/static.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   res.send("Welcome to Bloccit");
 });
+router.get("/marco", (req, res, next) => {
+  res.send("polo");
+});
 
 module.exports = router;
