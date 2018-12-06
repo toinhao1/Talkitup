0c35fd814d369afcad72813fa2394f0d5a0a072f
toinhao1 <toinhao89@gmail.com>
2018-12-06 08:57:35 -0600

    Checkpoint 4 RCV Complete

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
 
-modeule.exports = {
-  init() {
-
+module.exports = {
+  init(app, express) {
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(express.static(path.join(__dirname, "..", "assets")));
   }
 };
